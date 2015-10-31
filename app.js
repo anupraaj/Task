8f9e1ab70c8418c3ae0dc5988a217c729fcdfe1e
vinay588 <vinn.sharma@yahoo.com>
2015-10-31 18:39:34 +0530

    First commit

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 (function () {
 
     var app = angular.module('timeApp', []);
-    app.controller('TimeController', function ($http) {
-
+    app.controller('TimeController', function ($scope, $http) {
         var time = this;
-
         time.timings = [];
 
-        $http.get('timings.json').success(function (data) {
 
+
+
+
+        $http.get('timings.json').success(function (data) {
             time.timings = data;
+
+
+
+
+
         });
     });
 })();
